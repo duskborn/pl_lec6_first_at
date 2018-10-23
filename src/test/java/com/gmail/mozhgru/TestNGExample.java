04f843e0460b9a14e89076a78491c0d59c428a9c
username <mozhgru@gmail.com>
2018-10-23 13:15:52 +0400

    Added negative test for login

--- src/test/java/com/gmail/mozhgru/TestNGExample.java
+++ src/test/java/com/gmail/mozhgru/TestNGExample.java
@@ -16,6 +16,17 @@ public class TestNGExample {
     private WebDriver driver;
     private WebDriverWait wait;
 
+    public void loginTry(String login, String pass) {
+        driver.findElement(By.id("login-username")).sendKeys(login);
+        driver.findElement(By.cssSelector("#login-password")).sendKeys(pass);
+        driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
+    }
+
+    public void clearLogin() {
+        driver.findElement(By.id("login-username")).clear();
+        driver.findElement(By.cssSelector("#login-password")).clear();
+    }
+
     @BeforeTest
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\mozhg\\jars\\chromedriver.exe");
@@ -28,19 +39,29 @@ public class TestNGExample {
         driver.quit();
     }
 
-    @Test
-    public void loginTest(){
+    @Test (priority=1)
+    public void launchMatrixBoard() {
         driver.get("http://at.pflb.ru/matrixboard2/");
-        WebElement loginField = driver.findElement(By.id("login-username"));
-        WebElement passwordField = driver.findElement(By.cssSelector("#login-password"));
-        WebElement loginButton = driver.findElement(By.xpath("//*[@id=\"login-button\"]"));
+    }
 
-        loginField.sendKeys("user");
-        passwordField.sendKeys("user");
-        loginButton.click();
+    @Test (priority=2)
+    public void notValidLoginTest(){
+        loginTry("wrong_login", "wrong_password");
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#login-failed")));
+        WebElement loginFailed = driver.findElement(By.cssSelector("#login-failed"));
+        Assert.assertEquals(loginFailed.getText(), "Неправильный логин или пароль.");
+
+        clearLogin();
+    }
+
+    @Test (priority=3)
+    public void validLoginTest(){
+        loginTry("user", "user");
 
         wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("#profile span")));
         WebElement userNameContainer = driver.findElement(By.cssSelector("#profile span"));
-        Assert.assertEquals("user", userNameContainer.getText());
+        Assert.assertEquals(userNameContainer.getText(), "user");
     }
+
 }
